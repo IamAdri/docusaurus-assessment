3d4a4c9c2ffe6ae517ce42d95d3db539923630ca
IamAdri <atoma304@gmail.com>
2026-02-09 12:20:18 +0200

    Change the url

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://docusaurus-assessment.vercel.app/",
+  url: "https://docusaurus-assessment.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
